23a22de3b06174930328007685d1775d13df260e
pixel-glitch-dev <pixelrushyt04@gmail.com>
2025-10-17 05:19:23 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-const carousel = document.querySelector(".carousel");
-const images = document.querySelectorAll(".carousel img");
-const prev = document.querySelector(".arrow.left");
-const next = document.querySelector(".arrow.right");
+const images = document.querySelectorAll(".carousel-images img");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
 
 let index = 0;
 
-function showSlide(i) {
-  index = (i + images.length) % images.length;
-  carousel.style.transform = `translateX(-${index * 100}%)`;
+function showImage(i) {
+  const total = images.length;
+  if (i < 0) index = total - 1;
+  else if (i >= total) index = 0;
+  else index = i;
+
+  const offset = -index * 100;
+  document.querySelector(".carousel-images").style.transform = `translateX(${offset}%)`;
 }
 
-next.addEventListener("click", () => showSlide(index + 1));
-prev.addEventListener("click", () => showSlide(index - 1));
+// Button controls
+prevBtn.addEventListener("click", () => showImage(index - 1));
+nextBtn.addEventListener("click", () => showImage(index + 1));
 
+// Swipe support
 let startX = 0;
-carousel.addEventListener("touchstart", e => startX = e.touches[0].clientX);
-carousel.addEventListener("touchend", e => {
-  const diff = e.changedTouches[0].clientX - startX;
-  if (diff > 50) showSlide(index - 1);
-  else if (diff < -50) showSlide(index + 1);
+document.querySelector(".carousel").addEventListener("touchstart", (e) => {
+  startX = e.touches[0].clientX;
+});
+
+document.querySelector(".carousel").addEventListener("touchend", (e) => {
+  const endX = e.changedTouches[0].clientX;
+  if (startX - endX > 50) showImage(index + 1);
+  if (endX - startX > 50) showImage(index - 1);
 });
