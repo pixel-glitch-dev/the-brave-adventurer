3dbc930676bf1ae5cece31a8675b3dc7b11c8fc4
pixel-glitch-dev <pixelrushyt04@gmail.com>
2025-10-17 04:09:10 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function showSlide() {
   slides.style.transform = `translateX(${-index * 100}%)`;
 }
 
+// Button navigation
 next.addEventListener("click", () => {
   index = (index + 1) % totalSlides;
   showSlide();
@@ -18,6 +19,33 @@ prev.addEventListener("click", () => {
   index = (index - 1 + totalSlides) % totalSlides;
   showSlide();
 });
+
+// Swipe support for mobile
+let startX = 0;
+let endX = 0;
+
+slides.addEventListener("touchstart", (e) => {
+  startX = e.touches[0].clientX;
+});
+
+slides.addEventListener("touchend", (e) => {
+  endX = e.changedTouches[0].clientX;
+  handleSwipe();
+});
+
+function handleSwipe() {
+  const diff = startX - endX;
+  if (Math.abs(diff) > 50) { // prevent accidental triggers
+    if (diff > 0) {
+      // Swipe left
+      index = (index + 1) % totalSlides;
+    } else {
+      // Swipe right
+      index = (index - 1 + totalSlides) % totalSlides;
+    }
+    showSlide();
+  }
+}
 
 // Resize fix for mobile
 window.addEventListener("resize", showSlide);
